639e061e0f363ee7f4581773359e787a06180c95
sumitbokshi <bokshisumit@gmail.com>
2022-05-08 13:18:51 +0530

    womensPage

--- womensPage/womens.js
+++ womensPage/womens.js
@@ -1,6 +1,6 @@
-//   DATA   WOMEN
 
-let data = [
+
+var data = [
     {
         img_1: "https://www.theoutnet.com/variants/images/46376663162746980/F/w1020_q80.jpg",
         img_2: "https://www.theoutnet.com/variants/images/46376663162746980/R/w1020_q80.jpg",
@@ -44,17 +44,17 @@ let data = [
         brand: "ALICE + OLIVIA",
         description: "Kenley cotton-blend jersey wide-leg pants ",
         price: 187,
-        strikedoffprice: 341,
-        discount: 45,
+        strikedoffprice: 500,
+        discount: 60,
     },
     {
         img_1: "https://www.theoutnet.com/variants/images/42247633208262636/F/w720_q80.jpg",
         img_2: "https://www.theoutnet.com/variants/images/42247633208262636/R/w1020_q80.jpg",
         brand: "BALMAIN",
         description: "Crepon-paneled wool-blend tweed jacket",
-        price: 1434,
+        price: 2005,
         strikedoffprice: 3186,
-        discount: 55,
+        discount: 37,
     },
 
     {
@@ -115,7 +115,7 @@ let data = [
         img_1: "https://www.theoutnet.com/variants/images/1647597276258123/F/w720_q80.jpg",
         img_2: "https://www.theoutnet.com/variants/images/1647597276258123/R/w1020_q80.jpg",
         brand: "STUDIOS",
-        description: "https://www.theoutnet.com/variants/images/1647597276258123/R/w1020_q80.jpg ",
+        description: "Striped Coat",
         price: 182,
         strikedoffprice: 365,
         discount: 50,
@@ -274,5 +274,269 @@ let data = [
         discount: 50,
     },
 
+];
 
-]
+localStorage.setItem("data1", JSON.stringify(data));
+
+var womenData = JSON.parse(localStorage.getItem("data1")) || []
+
+function displayDataWomen(womenData) {
+    document.querySelector("#products").innerHTML = ""
+    womenData.map(function (elem) {
+        var maindiv = document.createElement("div")
+        maindiv.setAttribute("class", "maindiv")
+        var div1 = document.createElement("div")
+        div1.setAttribute("class", "priceDiv")
+        var div2 = document.createElement("div")
+        div2.setAttribute("id", "infoDiv")
+        var image = document.createElement("img")
+        image.setAttribute("src", elem.img_2)
+        image.addEventListener("click", function () {
+            moreInfo(elem);
+            window.location.href = "#";
+        })
+        var brand = document.createElement("h3")
+        brand.innerText = elem.brand
+        brand.style.fontWeight = "bold"
+
+        var description = document.createElement("p")
+        description.innerText = elem.description
+
+        var price = document.createElement("p")
+        price.innerText = "$" + elem.price
+        price.style.fontWeight = "bold"
+
+        var strikedoffprice = document.createElement("p")
+        strikedoffprice.innerText = "$" + elem.strikedoffprice
+        strikedoffprice.style.textDecoration = "line-through"
+
+        var discount = document.createElement("p")
+        discount.innerText = "(" + elem.discount + "% OFF)"
+        discount.style.color = "red"
+
+
+
+        div1.append(price, strikedoffprice, discount)
+        div2.append(brand, description, div1)
+        maindiv.append(image, div2)
+        document.querySelector("#products").append(maindiv)
+    })
+}
+displayDataWomen(womenData)
+
+// *************sort*************
+
+function sorting() {
+    var prices = document.querySelector("#sortby").value;
+    console.log(prices)
+    if (prices == "hightolow") {
+        womenData.sort(function (a, b) {
+            return Number(b.price) - Number(a.price);
+        });
+        displayDataWomen(womenData)
+    }
+    if (prices == "lowtohigh") {
+        womenData.sort(function (a, b) {
+            return Number(a.price) - Number(b.price);
+        })
+        displayDataWomen(womenData)
+    }
+}
+
+
+//------------brand-filter-start----------//
+
+function alice() {
+    var value = document.querySelector("#alice").checked;
+    console.log(value);
+    if (value == true) {
+        var alice_item = womenData.filter(function (elem, index) {
+            return elem.brand == "ALICE + OLIVIA";
+        });
+        displayDataWomen(alice_item);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+function balmain() {
+    var value = document.querySelector("#balmain").checked;
+    console.log(value)
+    if (value == true) {
+        var balmain_item = womenData.filter(function (elem, index) {
+            return elem.brand == "BALMAIN";
+        });
+        displayDataWomen(balmain_item);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+function helmut() {
+    var value = document.querySelector("#helmut").checked;
+    console.log(value);
+    if (value == true) {
+        var helmut_item = womenData.filter(function (elem, index) {
+            return elem.brand == "HELMUT LANG";
+        });
+        displayDataWomen(helmut_item);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+function studios() {
+    var value = document.querySelector("#studios").checked;
+    console.log(value);
+    if (value == true) {
+        var studios_item = womenData.filter(function (elem, index) {
+            return elem.brand == "STUDIOS";
+        });
+        displayDataWomen(studios_item);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+function frame() {
+    var value = document.querySelector("#frame").checked;
+    console.log(value);
+    if (value == true) {
+        var frame_item = womenData.filter(function (elem, index) {
+            return elem.brand == "FRAME";
+        });
+        displayDataWomen(frame_item);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+function koral() {
+    var value = document.querySelector("#koral").checked;
+    console.log(value);
+    if (value == true) {
+        var koral_item = womenData.filter(function (elem, index) {
+            return elem.brand == "KORAL";
+        });
+        displayDataWomen(koral_item);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+            // price filter
+
+function $50to$100() {
+    var $ = document.querySelector("#50to100").checked;
+    if ($ == true) {
+        var $50to$100 = womenData.filter(function (elem) {
+            if (elem.price > 50  && elem.price <= 100) {
+                return elem.price;
+            }
+        });
+        displayDataWomen($50to$100);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+
+function Rs1199toRs2098() {
+    var Rs = document.querySelector("#Rs1199toRs2098").checked;
+    if (Rs == true) {
+        var Rs1199toRs2098 = womenData.filter(function (elem) {
+            if (elem.price > 1199 && elem.price <= 2098) {
+                return elem.price;
+            }
+        });
+        displayDataWomen(Rs1199toRs2098);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+function Rs2098toRs3119() {
+    var Rs = document.querySelector("#Rs2098toRs3119").checked;
+    if (Rs == true) {
+        var Rs2098toRs3119 = womenData.filter(function (elem) {
+            if (elem.price > 2098 && elem.price <= 3119) {
+                return elem.price;
+            }
+        });
+        displayDataWomen(Rs2098toRs3119);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+function Rs3119toRsabove() {
+    var Rs = document.querySelector("#Rs3119toRsabove").checked;
+    if (Rs == true) {
+        var Rs3119toRsabove = womenData.filter(function (elem) {
+            if (elem.price > 3119) {
+                return elem.price;
+            }
+        });
+        displayDataWomen(Rs3119toRsabove);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+//----------price-filter-end--------------//
+
+//---------discount-filter-start-----------//
+
+
+
+function above50() {
+    var above50 = document.querySelector("#above50").checked;
+    if (above50 == true) {
+        var above50 = womenData.filter(function (elem) {
+            if (elem.discount >= 50) {
+                return elem.discount;
+            }
+        });
+        displayDataWomen(above50);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+function above60() {
+    var above60 = document.querySelector("#above60").checked;
+    if (above60 == true) {
+        var above60 = womenData.filter(function (elem) {
+            if (elem.discount >= 60) {
+                return elem.discount;
+            }
+        });
+        displayDataWomen(above60);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+function above70() {
+    var above70 = document.querySelector("#above70").checked;
+    if (above70 == true) {
+        var above70 = womenData.filter(function (elem) {
+            if (elem.discount >= 70) {
+                return elem.discount;
+            }
+        });
+        displayDataWomen(above70);
+    } else {
+        displayDataWomen(womenData)
+    }
+}
+
+
+
+
+
+
+
+
+
+
